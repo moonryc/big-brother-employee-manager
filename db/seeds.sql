7e523a4b37fa256c9f339119532b15f12472ac9f
moonryc <70920813+moonryc@users.noreply.github.com>
2022-02-08 22:12:40 -0600

    better seed examples for example video

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,8 +11,8 @@ VALUES ('Engineer',10000,1),
        ('Salezer',10,4);
 
 INSERT INTO employee (first_name,last_name ,role_id,manager_id)
-VALUES ('ryan','moon',1,null),
-       ('steve','moon',1,null),
-       ('travis','anderson',2,null),
-       ('sara','anakazam',4,null),
-       ('zoey','owah',3,null);
+VALUES ('ryan','moon',1,2),
+       ('Mr','CEO',1,null),
+       ('travis','anderson',2,1),
+       ('sara','anakazam',4,2),
+       ('zoey','owah',3,4);
