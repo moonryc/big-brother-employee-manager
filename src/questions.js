9bc0f363d5be0cb12455a6d639568ad4c627541a
moonryc <70920813+moonryc@users.noreply.github.com>
2022-02-08 22:13:58 -0600

    consolidated code

--- src/questions.js
+++ src/questions.js
@@ -4,83 +4,15 @@ const addDepartmentQuestions = [
         type: "input",
         name: "name",
         validate: (input) => {
-            if (input) {
+            if (input && input.length <31) {
                 return true
             }
-            console.log("please enter a valid name")
+            console.log("Please enter a valid name less than 30 characters")
             return false
         }
-    }
-]
-
-const addEmployeeQuestions =[
-    {
-        message:"What is the Employees first name",
-        name:"firstName",
-        type:"input",
-        validate:(input)=>{
-            if(input){
-                return true
-            }
-            console.log("Please enter a valid name")
-            return false
-        }
-    },
-    {
-        message:"What is the Employees last name",
-        name:"lastName",
-        type:"input",
-        validate:(input)=>{
-            if(input){
-                return true
-            }
-            console.log("Please enter a valid name")
-            return false
-        }
-    },
-    {
-        message:"What is the Employees role id",
-        name:"role_id",
-        type:"input",
-    },
-    {
-        message:"What is the Employees manager id",
-        name:"manager_id",
-        type:"input",
-        validate:(input)=>{
-            if(input){
-                return true
-            }
-            console.log("Please enter a valid name")
-            return false
-        }
-    }
-]
-
-const addRoleQuestions =[
-    {
-        message:"What is the Role title?",
-        name:"title",
-        type:"input",
-        validate:(input)=>{
-            if(input){
-                return true
-            }
-            console.log("Please enter a valid name")
-            return false
-        }
-    },
-    {
-        message:"What is the role's salary",
-        name:"salary",
-        type:"number",
-    },
-    {
-        message: "what department Id",
-        name:"department_id"
     }
 ]
 
 module.exports = {
-    addDepartmentQuestions, addEmployeeQuestions, addRoleQuestions
+    addDepartmentQuestions
 }
